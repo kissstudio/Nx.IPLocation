fc555877fffd72481595be258323a9c3526e2d6e
tiny <tiny@sandsea.info>
2015-08-12 20:54:20 +0800

    init

---
+++ QQWry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nx.IPLocation
+{
+    public class QQWry
+    {
+        private static IPScanner scanner = new IPScanner();
+        public static IpLocation Query(string ipAddress)
+        {
+            return scanner.Query(ipAddress);
+        }
+    }
+}
